e29b62196e1ae2c82f0a09ad41df3a00d24ca8ca
zio69exe <ziovanib@gmail.com>
2023-12-14 16:24:13 +0700

    indexphp

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+    <div style="margin: 10%; margin-top: 5%;">
+    <table class="table table-striped" style="align-items: center; text-align:center;">
+
+        <tr style="border: 1px solid black; border-radius: 10px;">
+            <th style="border: 1px solid black; border-radius: 10px;">#</th>
+            <td>hello</td>
+            <td>word</td>
+            <td>word</td>
+        </tr>
+
+        <tr>
+            <th>#</th>
+            <td>hello</td>
+            <td>word</td>
+            <td>word</td>
+        </tr>
+
+    </table>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</html>
